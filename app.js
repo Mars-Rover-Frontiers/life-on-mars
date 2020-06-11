5292a1a6a21d562ff13b5e5bdf974afa704ac2ba
AllanLimitless <allanlimitless55@gmail.com>
2020-06-11 16:31:25 -0400

    Added Rover object

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-console.log("Hello World!");
+/* 
+
+Variables
+
+currentRover = '';
+
+........................
+api key  
+o50slHLqJd3LnaWMAegD0nN5q83KIAw7CicARCKX
+
+
+
+in app 
+
+
+
+when user clicks on nav link, show element
+onclick section.show()
+
+scrolls down to the section
+section.animate
+
+
+
+*/
+
+
+let app = {};
+ 
+app.apiKey = 'o50slHLqJd3LnaWMAegD0nN5q83KIAw7CicARCKX';
+
+app.url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos';
+
+
+// Objects 
+app.curiosity = {
+    name: "curiosity",
+    cameras: ['FHAZ', 'RHAZ', 'MAST', 'CHEMCAM','MAHALI','MARDI','NAVCAM']  
+};
+
+app.spirit = {
+    name: "spirit",
+    cameras: ['FHAZ', 'RHAZ', 'NAVCAM', 'PANCAM', 'MINITES']
+};
+
+app.opportunity = {
+    name: "opportunity",
+    cameras: ['FHAZ', 'RHAZ', 'NAVCAM', 'PANCAM', 'MINITES']
+};
+
+
+
+
+
