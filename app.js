cfa8c6744c5784db2285e2469a2730e3d3f64e41
Satvir Sandhu <356Sandhu@gmail.com>
2020-06-13 11:33:54 -0400

    added form event listener

--- app.js
+++ app.js
@@ -23,82 +23,87 @@ section.animate
 */
 let app = {};
 
-$(document).ready(function () {      
+$(document).ready(function () {
+  app.apiKey = "o50slHLqJd3LnaWMAegD0nN5q83KIAw7CicARCKX";
 
- 
-app.apiKey = 'o50slHLqJd3LnaWMAegD0nN5q83KIAw7CicARCKX';
+  app.url =
+    "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=FHAZ&api_key=o50slHLqJd3LnaWMAegD0nN5q83KIAw7CicARCKX";
 
-app.url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=FHAZ&api_key=o50slHLqJd3LnaWMAegD0nN5q83KIAw7CicARCKX';
+  // url
 
-app.formSection = $('.formSection'); 
+  app.formSection = $(".formSection");
 
-app.roverHeading = $('#roverName');
+  app.roverHeading = $("#roverName");
 
-app.cameraSelection = $('#camera');
+  app.cameraSelection = $("#camera");
 
-app.makeRequest = function() {
-    console.log(this.url);
+  app.formSubmit = $("#roverButton");
+
+  app.makeRequest = function () {
     $.ajax({
-        url: this.url,
-        method: 'GET',
-        dataType: 'json'
-      }).then(function() {
-        console.log('It worked!');
-      }).catch(function() {
-          console.log('problem');
+      url: this.url,
+      method: "GET",
+      dataType: "json",
+    })
+      .then(function () {
+        console.log("Success: We got an image!");
+      })
+      .catch(function () {
+        console.log("Error: Something went wrong with the API request");
       });
-};
+  };
 
+  app.currentRover = "";
 
-// Objects 
-app.curiosity = {
-    element: $('#curiosityLink'),
+  app.currentCamera = "";
+
+  app.currentDate = "";
+
+  // Objects
+  app.curiosity = {
+    element: $("#curiosityLink"),
     name: "curiosity",
-    cameras: ['FHAZ', 'RHAZ', 'MAST', 'CHEMCAM','MAHALI','MARDI','NAVCAM']  
-};
+    cameras: ["FHAZ", "RHAZ", "MAST", "CHEMCAM", "MAHALI", "MARDI", "NAVCAM"],
+  };
 
-app.spirit = {
-    element: $('#spiritLink'),
+  app.spirit = {
+    element: $("#spiritLink"),
     name: "spirit",
-    cameras: ['FHAZ', 'RHAZ', 'NAVCAM', 'PANCAM', 'MINITES']
-};
+    cameras: ["FHAZ", "RHAZ", "NAVCAM", "PANCAM", "MINITES"],
+  };
 
-app.opportunity = {
-    element: $('#oppurtunityLink'),
+  app.opportunity = {
+    element: $("#oppurtunityLink"),
     name: "opportunity",
-    cameras: ['FHAZ', 'RHAZ', 'NAVCAM', 'PANCAM', 'MINITES']
-};
+    cameras: ["FHAZ", "RHAZ", "NAVCAM", "PANCAM", "MINITES"],
+  };
 
-
-app.loadRovers = function(rovers) {
+  app.loadRovers = function (rover) {
     app.formSection.show();
-    app.roverHeading.text(`rover: ${rovers.name}`);    
+    app.currentRover = rover.name;
+    console.log(`current rover changed: ${app.currentRover}`);
+    app.roverHeading.text(`rover: ${rover.name}`);
     app.cameraSelection.empty();
-    rovers.cameras.forEach(item => {
-        app.cameraSelection.append(`<option value=${item}>${item}</option>`);
-    }); 
-    app.makeRequest();
-};
+    rover.cameras.forEach((item) => {
+      app.cameraSelection.append(`<option value=${item}>${item}</option>`);
+    });
+  };
 
-app.curiosity.element.click(() => {
-    app.loadRovers(app.curiosity);    
-}); 
+  app.curiosity.element.click(() => {
+    app.loadRovers(app.curiosity);
+  });
 
-app.spirit.element.click(() => {
+  app.spirit.element.click(() => {
     app.loadRovers(app.spirit);
-});
+  });
 
-app.opportunity.element.click(() => {
+  app.opportunity.element.click(() => {
     app.loadRovers(app.opportunity);
+  });
+
+  app.formSubmit.click((e) => {
+    e.preventDefault();
+    console.log("button clicked");
+    app.makeRequest();
+  });
 });
-
-app.currentRover = '';
-
-app.currentCamera = '';
-
-app.currentDate = '';
-
-});
-
-
-
